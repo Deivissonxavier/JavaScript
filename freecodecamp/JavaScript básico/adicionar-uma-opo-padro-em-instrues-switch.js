4e012564e6266bd7570f8f6a1b0ef31f79832a83
Deivissonxavier <deivissonx3@gmail.com>
2023-03-18 14:10:32 -0300

    atualizando

---
+++ freecodecamp/JavaScript básico/adicionar-uma-opo-padro-em-instrues-switch.js
@@ -0,0 +1,23 @@
+function switchOfStuff(val) {
+  let answer = "";
+  // Altere apenas o código abaixo desta linha
+  switch(val){
+    case "a":
+      answer = "apple"
+      break;
+    case "b":
+      answer = "bird"
+      break;
+    case "c":
+      answer = "cat"
+      break;
+    default:
+      answer = "stuff"
+  }
+
+
+  // Altere apenas o código acima desta linha
+  return answer;
+}
+
+switchOfStuff(1);
---
+++ freecodecamp/JavaScript básico/selecionar-entre-vrias-opes-com-instrues-switch.js
@@ -0,0 +1,24 @@
+function caseInSwitch(val) {
+  let answer = "";
+  // Altere apenas o código abaixo desta linha
+  switch(val){
+    case 1:
+      answer = "alpha"
+      break;
+    case 2:
+      answer = "beta"
+      break;
+    case 3:
+      answer = "gamma"
+      break;
+    case 4:
+      answer = "delta"
+      break;
+  }
+
+
+  // Altere apenas o código acima desta linha
+  return answer;
+}
+
+caseInSwitch(1);
